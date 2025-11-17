2dd3fe6b9109852a0063370b5f28afbabe93c44c
Amirhon <aamirhon391@gmail.com>
2025-11-17 12:22:18 +0500

    third commit

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from gc import get_objects
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from . import models
 
 # Create your views here.
@@ -8,5 +10,13 @@ def index(request):
     return render(request, 'courses.html', {'courses': courses})
 
 def single_course(request, course_id):
-    course = models.Course.objects.get(pk=course_id)
+    # OPTION 1
+    # try:
+    #     course = models.Course.objects.get(pk=course_id)
+    #     return render(request, 'single_course.html', {'course': course})
+    # except models.Course.DoesNotExist:
+    #     raise Http404()
+
+    # OPTION 2
+    course = get_object_or_404(models.Course, pk=course_id)
     return render(request, 'single_course.html', {'course': course})
